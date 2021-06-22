8b4791bf69728e3d182108c621039581da173714
= <=>
2021-06-22 07:56:20 -0300

    Primeiro Commit

---
+++ 01-String-Len-Tamanho-Posicao.py
@@ -0,0 +1,9 @@
+print("Aprendendo Python")
+print()
+nome="Aroldo F de Souza"
+print(nome)
+print("Total de caracteres no Nome.: ", len(nome))
+print()
+print()
+print("Os Índices em Python começa pelo valor 0 a sua posição")
+print(nome[0])
---
+++ 02-String-Concatenacao.py
@@ -0,0 +1,8 @@
+print('Concatenar Strings')
+print()
+nome="Aroldo "
+sobrenome="Souza"
+nome_completo=nome+sobrenome
+print(nome_completo)
+print(nome * 10)
+print("Linux \n" * 5)
